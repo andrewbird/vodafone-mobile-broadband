306ebb896ef73507a251b99aebdbe4fa9222e0c5
Andrew Bird <ajb@spheresystems.co.uk>
2009-09-28 18:19:16 +0000

    Remove pysqlite dependency, it's standard in python 2.5+

--- wader/vmc/contrib/axiom/scripts/pysql.py
+++ wader/vmc/contrib/axiom/scripts/pysql.py
@@ -3,7 +3,11 @@ import sys
 import readline # Imported for its side-effects
 import traceback
 
-from pysqlite2.dbapi2 import connect
+try:
+    from sqlite3.dbapi2 import connect
+except ImportError:
+    from pysqlite2.dbapi2 import connect
+
 from pprint import pprint
 
 con = connect(sys.argv[1])
