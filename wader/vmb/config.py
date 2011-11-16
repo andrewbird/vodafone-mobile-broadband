fc6128765cc24df414613abe9229a4a14fc1914d
Vicente Hernando Ara <vha@spheresystems.co.uk>
2011-11-16 04:16:10 +0100

    Gconf transform bcm config into vodafone-mobile-broadband one

--- wader/vmb/config.py
+++ wader/vmb/config.py
@@ -18,9 +18,49 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """Configuration singleton for GTK"""
 
-from wader.common.config import WaderConfig
+import gconf
+
 from wader.vmb.consts import GCONF_BASE_DIR
 
+from wader.common._gconf import GConfHelper
+from wader.common.config import WaderConfig
+
+
 DEFAULT_KEYS = ['profile', 'preferences', 'sms']
+
+
+class CheckOldConfig(GConfHelper):
+
+    def __init__(self):
+        self.old = '/apps/bcm'
+        self.new = GCONF_BASE_DIR
+        super(CheckOldConfig, self).__init__()
+
+    def check(self):
+        if self.client.dir_exists(self.new):
+            return
+
+        if not self.client.dir_exists(self.old):
+            return
+
+        # We are going to move old into new data.
+        dir_list = [self.old]
+        for d in dir_list:
+            dir_list.extend(self.client.all_dirs(d))
+
+        for d in dir_list:
+            entries = self.client.all_entries(d)
+
+            for en in entries:
+                key = en.get_key()
+                new_key = self.new + key[len(self.old):]
+                value = en.get_value()
+                if value:
+                    self.client.set(new_key, value)
+
+        # We remove old directory.
+        self.client.recursive_unset(self.old,
+                                    gconf.UNSET_INCLUDING_SCHEMA_NAMES)
+
 
 config = WaderConfig(keys=DEFAULT_KEYS, base_path=GCONF_BASE_DIR)
